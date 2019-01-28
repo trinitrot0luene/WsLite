ac3f64e0ca81a63234b1c053b82e26a245849c8a
Trinitrotoluene <pmichallet00@gmail.com>
2019-01-28 23:57:43 +0100

    Delete ByteExtensions.cs

--- src/WsLite/Socket/ByteExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WsLite.Socket
-{
-    internal static class ByteExtensions
-    {
-        public static bool IsBitSet(this byte source, int position) => (source & (1 << position)) != 0;
-    }
-}
